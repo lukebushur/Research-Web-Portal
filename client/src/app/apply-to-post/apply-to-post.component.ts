0c2573dd3552cc9708f6d3b93912abcc70b281ea
nachobrothernic <35301229+nachobrothernic@users.noreply.github.com>
2023-12-05 20:55:08 -0500

    for Prasanna, modifies applyToOpp

--- client/src/app/apply-to-post/apply-to-post.component.ts
+++ client/src/app/apply-to-post/apply-to-post.component.ts
@@ -1,4 +1,4 @@
-import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA, OnInit, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TextFieldComponent } from './text-field/text-field.component';
@@ -8,17 +8,35 @@ import { OpportunityComponent } from './opportunity/opportunity.component';
 @Component({
   selector: 'app-to-post',
   templateUrl: './apply-to-post.component.html',
-  imports: [TextFieldComponent, ImageComponent, OpportunityComponent],
+  imports: [TextFieldComponent, ImageComponent, OpportunityComponent, CommonModule],
   styleUrls: ['./apply-to-post.component.css'],
   standalone: true,
   schemas: [NO_ERRORS_SCHEMA]
 })
-export class ApplyToOpp {
+export class ApplyToOpp implements OnInit {
   title = 'applyToOpp';
 
-  constructor() {}
+  questions: Array<string> = [
+    "hello"
+  ]
+
+  @ViewChildren('app-text-field', {read: ViewContainerRef})
+  answers!: ViewContainerRef;
+
+  ngOnInit() {
+
+  }
+
+  constructor() {
+    // Get the questions for this application
+    // Set the questions
+
+    // createComponent
+    // 
+
+  }
   
   submitApp(id: any) {
-    console.log("submitted");
+    console.log(this.answers);
   }
 }
