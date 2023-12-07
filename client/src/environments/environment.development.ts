fb631c282dc65e2616a9a2db7111035935fb3d83
Sean Tierney <stierney0505@gmail.com>
2023-12-07 12:30:29 -0500

    Modified Address for environments and scrollable table

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 // change to port 3000 for docker deployment
 export const environment = {
-    registerApiUrl: 'http://localhost:5000/api/register',
-    apiUrl: 'http://localhost:5000/api',
-    ipUrl: 'http://localhost:5000'
+    registerApiUrl: 'http://test-tcp-load-d26601b88864c06b.elb.us-east-2.amazonaws.com:5000/api/register',
+    apiUrl: 'http://test-tcp-load-d26601b88864c06b.elb.us-east-2.amazonaws.com:5000/api',
+    ipUrl: 'http://test-tcp-load-d26601b88864c06b.elb.us-east-2.amazonaws.com:5000'
 };
