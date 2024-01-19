bac2857c00d0f6a27282b0b1dfbec99244a40827
lukebushur <luke7ajb@gmail.com>
2024-01-19 01:37:16 -0500

    Industry routes and controller files

---
+++ server/controllers/industry.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+const JWT = require('jsonwebtoken');
+const generateRes = require('../helpers/generateJSON');
+
+const getName = async (req, res) => {
+    const accessToken = req.header('Authorization').split(' ')[1];
+    const decodeAccessToken = JWT.verify(accessToken, process.env.SECRET_ACCESS_TOKEN);
+
+    //check if user exists
+    const user = await User.findOne({ email: decodeAccessToken.email });
+
+    //check if user type is faculty
+    if (user.userType.Type == process.env.INDUSTRY) {
+        return res.status(200).json(generateRes(true, 200, "INDUSTRY_USER_NAME", {
+            name: user.name,
+        }));
+    }
+    return res.status(400).json(generateRes(false, 400, "BAD_REQUEST", {}));
+}
+
+module.exports = {
+    getName,
+}
