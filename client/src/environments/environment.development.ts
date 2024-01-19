27e48443518bb174951bf20ec4ff347038ace808
lukebushur <luke7ajb@gmail.com>
2024-01-19 01:50:21 -0500

    fixed registerApiUrl

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 // change to port 3000 for docker deployment
 export const environment = {
-    registerApiUrl: 'http://127.0.0.1:5000/api',
+    registerApiUrl: 'http://127.0.0.1:5000/api/register',
     apiUrl: 'http://127.0.0.1:5000/api',
     ipUrl: 'http://127.0.0.1:5000/api'
 };
