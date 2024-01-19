edf634bdcbf8095518b1c3e2dbd439159619131f
lukebushur <luke7ajb@gmail.com>
2024-01-19 01:54:46 -0500

    industry dashboard service for getting industry data from the backend

---
+++ client/src/controllers/industry-dashboard-controller/industry-dashboard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../auth-controller/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IndustryDashboardService {
+  apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  getName() {
+    const headers = this.authService.getHeaders();
+
+    return this.http.get(`${this.apiUrl}/industry/getName`, { headers });
+  }
+}
