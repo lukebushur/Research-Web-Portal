1c754c41dff411653aaa1940d51d6eac1cf4615c
lukebushur <luke7ajb@gmail.com>
2024-01-19 01:55:33 -0500

    industry dashboard at /industry-dashboard in the router

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,24 +9,26 @@ import { LoginComponent } from './components/login/login.component';
 import { ApplyToOpp } from './components/apply-to-post/apply-to-post.component';
 import { PostProjectComponent } from './components/post_creation/posts.component';
 import { FacultyDashboardApplyComponent } from './components/faculty-accept/accept.component';
-import { DemoProjectsComponent  } from './components/demoProjects/demoProject.component';
+import { DemoProjectsComponent } from './components/demoProjects/demoProject.component';
 import { SignoutComponent } from './components/signout/signout.component';
 import { ViewApplicationComponent } from './components/view-application/view-application.component';
+import { IndustryDashboardComponent } from './components/industry-dashboard/industry-dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'signup', pathMatch: 'full' },
-  { path: 'signup', component: SignupComponent},
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  { path: 'faculty-dashboard', component: FacultyDashboardComponent, canActivate: [AuthGuard]},
-  { path: "confirm-email/:emailtoken", component: ConfirmEmailComponent, canActivate: [AuthGuard]},
-  { path: "login", component: LoginComponent},
-  { path: "apply-to-post", component: ApplyToOpp, canActivate: [AuthGuard]},
-  { path: "create-post", component: PostProjectComponent, canActivate: [AuthGuard]},
-  { path: "project/applications", component: FacultyDashboardApplyComponent, canActivate: [AuthGuard]},
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'faculty-dashboard', component: FacultyDashboardComponent, canActivate: [AuthGuard] },
+  { path: "confirm-email/:emailtoken", component: ConfirmEmailComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent },
+  { path: "apply-to-post", component: ApplyToOpp, canActivate: [AuthGuard] },
+  { path: "create-post", component: PostProjectComponent, canActivate: [AuthGuard] },
+  { path: "project/applications", component: FacultyDashboardApplyComponent, canActivate: [AuthGuard] },
   { path: "demoProjects", component: DemoProjectsComponent },
   { path: "signout", component: SignoutComponent },
+  { path: "industry-dashboard", component: IndustryDashboardComponent, canActivate: [AuthGuard] },
   //This route has two URL parameters, one for projectID, and one for applicationID. It is used to access a specific applicant's data by the faculty
-  { path: "application/:projectID/:applicationID", component: ViewApplicationComponent} 
+  { path: "application/:projectID/:applicationID", component: ViewApplicationComponent }
 ];
 
 @NgModule({
