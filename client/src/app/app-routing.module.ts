5ef4758199da1d529245f438fafe6626e14c1af9
Sean Tierney <stierney0505@gmail.com>
2024-01-23 14:48:53 -0500

    Creation Minimum Post Creation Page - No unit tests currently

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,24 +9,24 @@ import { LoginComponent } from './components/login/login.component';
 import { ApplyToOpp } from './components/apply-to-post/apply-to-post.component';
 import { PostProjectComponent } from './components/post_creation/posts.component';
 import { FacultyDashboardApplyComponent } from './components/faculty-accept/accept.component';
-import { DemoProjectsComponent  } from './components/demoProjects/demoProject.component';
+import { DemoProjectsComponent } from './components/demoProjects/demoProject.component';
 import { SignoutComponent } from './components/signout/signout.component';
 import { ViewApplicationComponent } from './components/view-application/view-application.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'signup', pathMatch: 'full' },
-  { path: 'signup', component: SignupComponent},
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  { path: 'faculty-dashboard', component: FacultyDashboardComponent, canActivate: [AuthGuard]},
-  { path: "confirm-email/:emailtoken", component: ConfirmEmailComponent, canActivate: [AuthGuard]},
-  { path: "login", component: LoginComponent},
-  { path: "apply-to-post", component: ApplyToOpp, canActivate: [AuthGuard]},
-  { path: "create-post", component: PostProjectComponent, canActivate: [AuthGuard]},
-  { path: "project/applications", component: FacultyDashboardApplyComponent, canActivate: [AuthGuard]},
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'faculty-dashboard', component: FacultyDashboardComponent, canActivate: [AuthGuard] },
+  { path: "confirm-email/:emailtoken", component: ConfirmEmailComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent },
+  { path: "apply-to-post", component: ApplyToOpp, canActivate: [AuthGuard] },
+  { path: "create-post", component: PostProjectComponent, canActivate: [AuthGuard] },
+  { path: "project/applications", component: FacultyDashboardApplyComponent, canActivate: [AuthGuard] },
   { path: "demoProjects", component: DemoProjectsComponent },
   { path: "signout", component: SignoutComponent },
   //This route has two URL parameters, one for projectID, and one for applicationID. It is used to access a specific applicant's data by the faculty
-  { path: "application/:projectID/:applicationID", component: ViewApplicationComponent} 
+  { path: "application/:projectID/:applicationID", component: ViewApplicationComponent },
 ];
 
 @NgModule({
---
+++ client/src/app/components/post_creation/custom-question/custom-question.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomQuestionComponent } from './custom-question.component';
+
+describe('CustomQuestionComponent', () => {
+  let component: CustomQuestionComponent;
+  let fixture: ComponentFixture<CustomQuestionComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CustomQuestionComponent]
+    });
+    fixture = TestBed.createComponent(CustomQuestionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
