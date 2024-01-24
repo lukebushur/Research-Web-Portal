385e24f7bf9484f79df329295706b29eb9f061bc
Sean Tierney <stierney0505@gmail.com>
2024-01-24 01:02:03 -0500

    Extremely Minor Change moving a tag down in the html (noticed while testing before commiting)

--- client/src/app/components/post_creation/custom-question/custom-question.component.spec.ts
+++ client/src/app/components/post_creation/custom-question/custom-question.component.spec.ts
@@ -15,7 +15,43 @@ describe('CustomQuestionComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CustomQuestionComponent); // Make sure to replace 'MyComponent' with the actual name of your component
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Add more test cases as needed for your component's functionality
+
+  it('should add an option to the radio button options', () => {
+    component.type = 'radio buttons';
+    const initialOptionsLength = component.options.controls.length;
+
+    component.addOption();
+
+    expect(component.options.controls.length).toBe(initialOptionsLength + 1);
+  });
+
+  it('should remove an option from the radio button options', () => {
+    component.type = 'radio buttons';
+    const initialOptionsLength = component.options.controls.length;
+
+    component.removeOption(0);
+
+    expect(component.options.controls.length).toBe(initialOptionsLength - 1);
+  });
+
+  // Add more test cases for other functionalities
+
+  it('should delete the form', () => {
+    spyOn(component, 'onDelete');
+    const deleteButton = fixture.debugElement.nativeElement.querySelector('button');
+    deleteButton.click();
+
+    expect(component.onDelete).toHaveBeenCalled();
   });
 });
