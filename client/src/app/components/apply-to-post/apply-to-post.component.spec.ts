9ea215d7400d5b4f01e3beea9ef842f9ff42b27c
lukebushur <luke7ajb@gmail.com>
2024-01-28 16:40:42 -0500

    Updated applyToPost component test to conform to standard component test format

--- client/src/app/components/apply-to-post/apply-to-post.component.spec.ts
+++ client/src/app/components/apply-to-post/apply-to-post.component.spec.ts
@@ -1,29 +1,25 @@
-import { TestBed } from '@angular/core/testing';
-import { ApplyToOpp } from './apply-to-post.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ApplyToPostComponent } from './apply-to-post.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ApplyToOpp],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(ApplyToOpp);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'applyToOpp' title`, () => {
-    const fixture = TestBed.createComponent(ApplyToOpp);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('applyToOpp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(ApplyToOpp);
+  let component: ApplyToPostComponent;
+  let fixture: ComponentFixture<ApplyToPostComponent>;
+  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ApplyToPostComponent],
+    });
+    fixture = TestBed.createComponent(ApplyToPostComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('First Opportunity');
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have the "applyToOpp" title', () => {
+    expect(component.title).toEqual('applyToOpp');
   });
 });
