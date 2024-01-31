cfc3f3cec9d7c3d317e47a81444dc6e98bbad856
lukebushur <luke7ajb@gmail.com>
2024-01-31 02:33:13 -0500

    create jobs route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { DemoProjectsComponent } from './components/demoProjects/demoProject.com
 import { SignoutComponent } from './components/signout/signout.component';
 import { ViewApplicationComponent } from './components/view-application/view-application.component';
 import { IndustryDashboardComponent } from './components/industry-dashboard/industry-dashboard.component';
+import { CreateJobComponent } from './components/create-job/create-job.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'signup', pathMatch: 'full' },
@@ -27,6 +28,7 @@ const routes: Routes = [
   { path: "demoProjects", component: DemoProjectsComponent },
   { path: "signout", component: SignoutComponent },
   { path: "industry-dashboard", component: IndustryDashboardComponent, canActivate: [AuthGuard] },
+  { path: "create-job", component: CreateJobComponent, canActivate: [AuthGuard] },
   //This route has two URL parameters, one for projectID, and one for applicationID. It is used to access a specific applicant's data by the faculty
   { path: "application/:projectID/:applicationID", component: ViewApplicationComponent },
 ];
