c462091714050c842e0f8a52fa60a14221eee425
Luke Bushur <97570706+lukebushur@users.noreply.github.com>
2024-02-01 12:23:30 -0500

    Student Dashboard Applications Preview Backend Route + Critical Fixes (#24)
    * WIP get recent applications route
    
    * WIP get top applications after sorting array
    
    * Fixed HTTP requests not sending
    
    * added accountType to submission data
    
    * Backend route for getting top n applications written
    
    * fixed bug with getting user type
    
    * Removed unnecessary testing module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ConfirmEmailComponent } from './components/confirmemail/email.component';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 import { FacultyDashboardComponent } from './components/faculty-dashboard/faculty-dashboard.component';
@@ -73,7 +72,6 @@ import { CustomQuestionComponent } from './components/post_creation/custom-quest
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    HttpClientTestingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
