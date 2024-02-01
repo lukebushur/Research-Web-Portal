621a71c9a93a1518e8c1b13ddb48749b7c4b9f6a
lukebushur <luke7ajb@gmail.com>
2024-02-01 02:09:12 -0500

    Fixed base unit tests for job creation component and service

--- client/src/app/components/create-job/create-job.component.spec.ts
+++ client/src/app/components/create-job/create-job.component.spec.ts
@@ -1,6 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CreateJobComponent } from './create-job.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@Component({ standalone: true, selector: 'app-faculty-toolbar', template: '' })
+class FacultyToolbarStubComponent {}
 
 describe('CreateJobComponent', () => {
   let component: CreateJobComponent;
@@ -8,7 +22,20 @@ describe('CreateJobComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CreateJobComponent]
+      declarations: [CreateJobComponent],
+      imports: [
+        HttpClientTestingModule,
+        MatSnackBarModule,
+        FacultyToolbarStubComponent,
+        MatFormFieldModule,
+        MatRadioModule,
+        MatChipsModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+      ],
     });
     fixture = TestBed.createComponent(CreateJobComponent);
     component = fixture.componentInstance;
--- client/src/app/controllers/create-job-controller/create-job.service.spec.ts
+++ client/src/app/controllers/create-job-controller/create-job.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CreateJobService } from './create-job.service';
 
@@ -6,7 +7,9 @@ describe('CreateJobService', () => {
   let service: CreateJobService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CreateJobService);
   });
 
