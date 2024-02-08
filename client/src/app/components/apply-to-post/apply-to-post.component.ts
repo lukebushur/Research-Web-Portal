1bb5e9c864f2e723cc7a4d592c1a6aa4b2604447
lukebushur <luke7ajb@gmail.com>
2024-02-08 00:09:48 -0500

    WIP apply to post component

--- client/src/app/components/apply-to-post/apply-to-post.component.ts
+++ client/src/app/components/apply-to-post/apply-to-post.component.ts
@@ -1,13 +1,12 @@
 import { Component, NO_ERRORS_SCHEMA, OnInit, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//import { RouterOutlet } from '@angular/router';
 import { TextFieldComponent } from './text-field/text-field.component';
 import { ImageComponent } from './image/image.component';
 import { OpportunityComponent } from './opportunity/opportunity.component';
-import { MatTableDataSource } from '@angular/material/table';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { FormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { ApplyToPostService } from 'src/app/controllers/apply-to-post/apply-to-post.service';
 
 @Component({
   selector: 'app-to-post',
@@ -18,33 +17,37 @@ import {MatIconModule} from '@angular/material/icon';
   schemas: [NO_ERRORS_SCHEMA]
 })
 export class ApplyToPostComponent implements OnInit {
-  title = 'applyToOpp';
+  // title = 'applyToOpp';
 
   //For side-nav opening
   opened = false;
 
-  questions: Array<string> = [
-    "hello"
-  ]
+  // questions: Array<string> = [
+  //   "hello"
+  // ]
+  project: any;
+  questions: any[];
 
-  @ViewChildren('app-text-field', {read: ViewContainerRef})
+  @ViewChildren('app-text-field', { read: ViewContainerRef })
   answer!: ViewContainerRef;
 
+  constructor(private applyService: ApplyToPostService) { }
+
   ngOnInit() {
-
+    this.applyService.getProjects().subscribe({
+      next: (response: any) => {
+        if (response.success) {
+          this.project = response.success.data[0];
+          this.questions = this.project.questions;
+          console.log(this.project);
+        }
+      },
+      error: (response: any) => {
+        console.log('Error getting projects.');
+      },
+    });
   }
 
-  constructor() {
-    // Get the questions for this application
-    // Set the questions
-
-    // createComponent
-    // 
-
-  }
-
-
-  
   submitApp(id: any) {
     console.log(this.answer);
   }
