d3fee17a83dbe26e7800ea143fbbbc4c584d62fb
Adam Hughes <adamhughesme1@gmail.com>
2024-02-16 16:40:37 -0500

    generated component

---
+++ client/src/app/profile-screen/profile-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-screen',
+  templateUrl: './profile-screen.component.html',
+  styleUrls: ['./profile-screen.component.css']
+})
+export class ProfileScreenComponent {
+
+}
