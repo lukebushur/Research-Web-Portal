830caf50a4834b0879caa996ac35db108c34a91d
lukebushur <luke7ajb@gmail.com>
2024-02-20 19:37:26 -0500

    Updated ProjectData Interface with correct fields

--- client/src/app/_models/apply-to-post/projectData.ts
+++ client/src/app/_models/apply-to-post/projectData.ts
@@ -1,16 +1,16 @@
 import { QuestionData } from "./questionData";
 
 export interface ProjectData {
-  projectName: string;
   professorName: string;
   professorEmail: string;
   projectID: string;
-  title: string;
+  projectName: string;
   description: string;
   categories: string[];
   GPA: number;
   majors: string[];
   posted: string;
   deadline: string;
+  responsibilities?: string;
   questions: QuestionData[];
 };
