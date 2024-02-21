79547a76ded4e50473a0c7d98ff5d573276d0c06
lukebushur <luke7ajb@gmail.com>
2024-02-20 20:59:38 -0500

    Implemented ApplyToPostService unit tests

--- client/src/app/controllers/apply-to-post/apply-to-post.service.spec.ts
+++ client/src/app/controllers/apply-to-post/apply-to-post.service.spec.ts
@@ -1,16 +1,67 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ApplyToPostService } from './apply-to-post.service';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth-controller/auth.service';
+import { of } from 'rxjs';
+import { ApplyRequestData } from 'src/app/_models/apply-to-post/applyRequestData';
 
 describe('ApplyToPostService', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let authService: AuthService;
   let service: ApplyToPostService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApplyToPostService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+    authService = new AuthService(httpClientSpy);
+    service = new ApplyToPostService(httpClientSpy, authService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return project information', (done: DoneFn) => {
+    const expectedResponse = {
+      success: {
+        project: {
+          projectName: 'Test Project',
+        }
+      }
+    };
+
+    httpClientSpy.post.and.returnValue(of(expectedResponse));
+
+    service.getProjectInfo({}).subscribe({
+      next: (data: any) => {
+        expect(data).withContext('expected response').toEqual(expectedResponse);
+        done();
+      },
+      error: done.fail,
+    });
+    expect(httpClientSpy.post.calls.count()).withContext('one call').toBe(1);
+  });
+
+  it('should return application successfully created response', (done: DoneFn) => {
+    const fakeApplyData: ApplyRequestData = {
+      projectID: '123',
+      professorEmail: 'fakeemail@email.com',
+      questions: [],
+    };
+    const expectedResponse = {
+      success: {
+        status: 200,
+        message: 'APPLICATION_CREATED',
+      }
+    };
+
+    httpClientSpy.post.and.returnValue(of(expectedResponse));
+
+    service.createApplication(fakeApplyData).subscribe({
+      next: (data: any) => {
+        expect(data).withContext('expected response').toEqual(expectedResponse);
+        done();
+      },
+      error: done.fail,
+    });
+    expect(httpClientSpy.post.calls.count()).withContext('one call').toBe(1);
   });
 });
