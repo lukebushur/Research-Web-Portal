7a0040def518409b8c33c058aadc08832d07ac86
lukebushur <luke7ajb@gmail.com>
2024-02-20 20:59:07 -0500

    Fixed unit tests

--- client/src/app/components/apply-to-post/apply-to-post.component.spec.ts
+++ client/src/app/components/apply-to-post/apply-to-post.component.spec.ts
@@ -2,6 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ApplyToPostComponent } from './apply-to-post.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 describe('ApplyToPostComponent', () => {
   let component: ApplyToPostComponent;
@@ -9,8 +14,13 @@ describe('ApplyToPostComponent', () => {
   
   beforeEach(() => {
     TestBed.configureTestingModule({
+      declarations: [ApplyToPostComponent],
       imports: [
-        ApplyToPostComponent,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatSnackBarModule,
+        MatSidenavModule,
+        MatListModule,
         BrowserAnimationsModule,
       ],
     });
@@ -21,9 +31,5 @@ describe('ApplyToPostComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should have the "applyToOpp" title', () => {
-    expect(component.title).toEqual('applyToOpp');
   });
 });
