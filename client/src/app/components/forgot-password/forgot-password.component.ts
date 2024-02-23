e355add6272ac6130080d048d8b308370f9bed37
lukebushur <luke7ajb@gmail.com>
2024-02-23 03:48:19 -0500

    Implemented forgot password component

---
+++ client/src/app/components/forgot-password/forgot-password.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/controllers/login-controller/login.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent {
+
+  // Reactive forgot password form
+  forgotForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(6),
+      Validators.maxLength(254),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(255),
+    ]),
+  });
+
+  // Whether the password should be hidden or shown
+  hide: boolean = true;
+
+  constructor(private router: Router, private loginService: LoginService) { }
+
+  // Error message for email based on validators
+  emailErrorMessage(): string {
+    if (this.forgotForm.get('email')?.hasError('required')) {
+      return 'You must enter an email.';
+    }
+    if (this.forgotForm.get('email')?.hasError('minlength')) {
+      return 'Minimum email length: 6';
+    }
+    if (this.forgotForm.get('email')?.hasError('maxlength')) {
+      return 'Maximum email length: 254';
+    }
+    return this.forgotForm.get('email')?.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  // Error message for password based on validators
+  passwordErrorMessage(): string {
+    if (this.forgotForm.get('password')?.hasError('required')) {
+      return 'You must enter an password.';
+    }
+    if (this.forgotForm.get('password')?.hasError('minlength')) {
+      return 'Minimum password length: 10';
+    }
+    if (this.forgotForm.get('password')?.hasError('maxlength')) {
+      return 'Maximum password length: 255';
+    }
+    return '';
+  }
+
+  onSubmit() {
+    const forgotData = {
+      email: this.forgotForm.value.email,
+      provisionalPassword: this.forgotForm.value.password,
+    };
+
+    this.loginService.forgotPassword(forgotData).subscribe({
+      next: (data: any) => {
+        if (data.success) {
+          console.log('success!');
+        }
+      },
+      error: (error) => {
+        console.error('error logging in', error);
+      },
+    });
+  }
+}
