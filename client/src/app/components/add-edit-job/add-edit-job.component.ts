5cbcde9b4c846ea9b0207cdbf03af436b8643f39
lukebushur <luke7ajb@gmail.com>
2024-03-01 01:37:59 -0500

    Added pre-hire assessment creation to job creation

--- client/src/app/components/add-edit-job/add-edit-job.component.ts
+++ client/src/app/components/add-edit-job/add-edit-job.component.ts
@@ -8,6 +8,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { JobCardData } from '../industry-dashboard/job-card/job-card-data';
 import { Location } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { AssessmentChooserComponent } from './assessment-chooser/assessment-chooser.component';
+import { QuestionData } from 'src/app/_models/apply-to-post/questionData';
 
 @Component({
   selector: 'app-add-edit-job',
@@ -15,24 +18,29 @@ import { Location } from '@angular/common';
   styleUrls: ['./add-edit-job.component.css']
 })
 export class AddEditJobComponent {
-  initialJobData: JobCardData;
+  initialJobData?: JobCardData;
   isCreateJob: boolean = true;
 
-  addEditForm = new FormGroup({
-    employer: new FormControl('', [Validators.required]),
-    title: new FormControl('', [Validators.required]),
-    isInternship: new FormControl<boolean | null>(null, [Validators.required]),
-    isFullTime: new FormControl<boolean | null>(null, [Validators.required]),
-    description: new FormControl('', [Validators.required]),
-    location: new FormControl('', [Validators.required]),
-    reqYearsExp: new FormControl<number | null>(null, [Validators.required, Validators.pattern(/^\d+$/)]),
-    tags: new FormArray([]),
-    timeCommitment: new FormControl(''),
-    pay: new FormControl(''),
-    deadline: new FormControl<Date | null>(null),
-    range: new FormGroup({
-      start: new FormControl<Date | null>(null),
-      end: new FormControl<Date | null>(null),
+  jobForm = new FormGroup({
+    jobDetails: new FormGroup({
+      employer: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.required]),
+      isInternship: new FormControl<boolean | null>(null, [Validators.required]),
+      isFullTime: new FormControl<boolean | null>(null, [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      location: new FormControl('', [Validators.required]),
+      reqYearsExp: new FormControl<number | null>(null, [Validators.required, Validators.pattern(/^\d+$/)]),
+      tags: new FormArray([]),
+      timeCommitment: new FormControl(''),
+      pay: new FormControl(''),
+      deadline: new FormControl<Date | null>(null),
+      range: new FormGroup({
+        start: new FormControl<Date | null>(null),
+        end: new FormControl<Date | null>(null),
+      }),
+    }),
+    questionsGroup: new FormGroup({
+      questions: new FormArray([]),
     }),
   });
 
@@ -46,6 +54,7 @@ export class AddEditJobComponent {
     private addEditJobService: AddEditJobService,
     private announcer: LiveAnnouncer,
     private snackBar: MatSnackBar,
+    public dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
@@ -59,42 +68,49 @@ export class AddEditJobComponent {
       next: (data: any) => {
         if (data.success) {
           this.initialJobData = JSON.parse(data.success.job);
-          console.log(this.initialJobData);
 
-          this.addEditForm.get('employer')?.setValue(this.initialJobData.employer);
-          this.addEditForm.get('title')?.setValue(this.initialJobData.title);
-          this.addEditForm.get('isInternship')?.setValue(this.initialJobData.isInternship);
-          this.addEditForm.get('isFullTime')?.setValue(this.initialJobData.isFullTime);
-          this.addEditForm.get('description')?.setValue(this.initialJobData.description);
-          this.addEditForm.get('location')?.setValue(this.initialJobData.location);
-          this.addEditForm.get('reqYearsExp')?.setValue(this.initialJobData.reqYearsExp);
-          for (const tag of this.initialJobData.tags ?? []) {
+          this.jobDetails.get('employer')?.setValue(this.initialJobData?.employer);
+          this.jobDetails.get('title')?.setValue(this.initialJobData?.title);
+          this.jobDetails.get('isInternship')?.setValue(this.initialJobData?.isInternship);
+          this.jobDetails.get('isFullTime')?.setValue(this.initialJobData?.isFullTime);
+          this.jobDetails.get('description')?.setValue(this.initialJobData?.description);
+          this.jobDetails.get('location')?.setValue(this.initialJobData?.location);
+          this.jobDetails.get('reqYearsExp')?.setValue(this.initialJobData?.reqYearsExp);
+          for (const tag of this.initialJobData?.tags ?? []) {
             this.tags.push(new FormControl(tag));
           }
-          this.addEditForm.get('timeCommitment')?.setValue(this.initialJobData.timeCommitment ?? null);
-          this.addEditForm.get('pay')?.setValue(this.initialJobData.pay ?? null);
-          const deadline = this.initialJobData.deadline ? new Date(this.initialJobData.deadline) : null;
-          this.addEditForm.get('deadline')?.setValue(deadline);
-          this.addEditForm.get('reqYearsExp')?.setValue(this.initialJobData.reqYearsExp);
-          const startDate = this.initialJobData.startDate ? new Date(this.initialJobData.startDate) : null;
+          this.jobDetails.get('timeCommitment')?.setValue(this.initialJobData?.timeCommitment ?? null);
+          this.jobDetails.get('pay')?.setValue(this.initialJobData?.pay ?? null);
+          const deadline = this.initialJobData?.deadline ? new Date(this.initialJobData.deadline) : null;
+          this.jobDetails.get('deadline')?.setValue(deadline);
+          this.jobDetails.get('reqYearsExp')?.setValue(this.initialJobData?.reqYearsExp);
+          const startDate = this.initialJobData?.startDate ? new Date(this.initialJobData.startDate) : null;
           this.range.get('start')?.setValue(startDate);
-          const endDate = this.initialJobData.endDate ? new Date(this.initialJobData.endDate) : null;
+          const endDate = this.initialJobData?.endDate ? new Date(this.initialJobData.endDate) : null;
           this.range.get('end')?.setValue(endDate);
         }
       }
     });
   }
 
+  get jobDetails() {
+    return this.jobForm.get('jobDetails') as FormGroup;
+  }
+
+  get jobQuestions() {
+    return this.jobForm.get('questionsGroup') as FormGroup;
+  }
+
   get range() {
-    return this.addEditForm.get('range') as FormGroup;
+    return this.jobDetails.get('range') as FormGroup;
   }
 
   get tags() {
-    return this.addEditForm.get('tags') as FormArray;
+    return this.jobDetails.get('tags') as FormArray;
   }
 
   get reqYearsExp() {
-    return this.addEditForm.get('reqYearsExp') as FormControl;
+    return this.jobDetails.get('reqYearsExp') as FormControl;
   }
 
   addTag(event: MatChipInputEvent): void {
@@ -137,10 +153,39 @@ export class AddEditJobComponent {
     this.location.back();
   }
 
-  onSubmit() {
-    console.log(this.addEditForm.value);
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AssessmentChooserComponent);
 
-    if (this.addEditForm.invalid) {
+    dialogRef.afterClosed().subscribe((questions: QuestionData[]) => {
+      for (const question of questions) {
+        const questionGroup = new FormGroup({
+          question: new FormControl(question.question, [Validators.required]),
+          requirementType: new FormControl(question.requirementType, [Validators.required]),
+          required: new FormControl(question.required, [Validators.required]),
+          choices: (question.choices) 
+            ? new FormArray((question.choices.map(choice => {
+              return new FormControl(choice, [Validators.required]);
+            })))
+            : new FormArray([new FormControl('', [Validators.required])]),
+        });
+        if (question.requirementType === 'text') {
+          questionGroup.get('choices')?.disable();
+        }
+        const questionsArray = this.jobQuestions.get('questions') as FormArray;
+        questionsArray.push(questionGroup);
+      }
+    });
+  }
+
+  clearQuestions(): void {
+    const questionsArray = this.jobQuestions.get('questions') as FormArray;
+    questionsArray.clear();
+  }
+
+  onSubmit() {
+    console.log(this.jobForm.value);
+
+    if (this.jobForm.invalid) {
       this.snackBar.open('1 or more invalid fields', 'Close', {
         duration: 5000,
       });
@@ -151,19 +196,20 @@ export class AddEditJobComponent {
     const data = {
       jobType: 'active',
       jobDetails: {
-        employer: this.addEditForm.get('employer')?.value,
-        title: this.addEditForm.get('title')?.value,
-        isInternship: this.addEditForm.get('isInternship')?.value,
-        isFullTime: this.addEditForm.get('isFullTime')?.value,
-        description: this.addEditForm.get('description')?.value,
-        location: this.addEditForm.get('location')?.value,
+        employer: this.jobDetails.get('employer')?.value,
+        title: this.jobDetails.get('title')?.value,
+        isInternship: this.jobDetails.get('isInternship')?.value,
+        isFullTime: this.jobDetails.get('isFullTime')?.value,
+        description: this.jobDetails.get('description')?.value,
+        location: this.jobDetails.get('location')?.value,
         reqYearsExp: parseInt(rye),
-        tags: this.addEditForm.get('tags')?.value,
-        timeCommitment: this.addEditForm.get('timeCommitment')?.value,
-        pay: this.addEditForm.get('pay')?.value,
-        deadline: this.addEditForm.get('deadline')?.value,
+        tags: this.jobDetails.get('tags')?.value,
+        timeCommitment: this.jobDetails.get('timeCommitment')?.value,
+        pay: this.jobDetails.get('pay')?.value,
+        deadline: this.jobDetails.get('deadline')?.value,
         startDate: this.range.get('start')?.value,
         endDate: this.range.get('end')?.value,
+        questions: this.jobQuestions.get('questions')?.value,
       },
     };
 
@@ -189,7 +235,7 @@ export class AddEditJobComponent {
     }
 
     this.addEditJobService.editJob({
-      jobId: this.initialJobData._id,
+      jobId: this.initialJobData!._id,
       ...data,
     }).subscribe({
       next: (response: any) => {
