49ca1a9e14f18fdc27e518570f560d859cf6f160
Adam Hughes <adamhughesme1@gmail.com>
2024-03-01 17:25:05 -0500

    fixed unit tests

--- client/src/app/components/edit-profile-screen/edit-profile-screen.component.spec.ts
+++ client/src/app/components/edit-profile-screen/edit-profile-screen.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditProfileScreenComponent } from './edit-profile-screen.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('EditProfileScreenComponent', () => {
   let component: EditProfileScreenComponent;
@@ -8,6 +14,15 @@ describe('EditProfileScreenComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+      ],
       declarations: [EditProfileScreenComponent]
     });
     fixture = TestBed.createComponent(EditProfileScreenComponent);
--- client/src/app/controllers/profile-controller/profile-service.service.spec.ts
+++ client/src/app/controllers/profile-controller/profile-service.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProfileServiceService } from './profile-service.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProfileServiceService', () => {
   let service: ProfileServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(ProfileServiceService);
   });
 
