79cb043f36204d01f6a52180ff6f60c8d9ca3fc7
Adam Hughes <adamhughesme1@gmail.com>
2024-03-01 16:45:20 -0500

    created submit profile function

--- client/src/app/controllers/profile-controller/profile-service.service.ts
+++ client/src/app/controllers/profile-controller/profile-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth-controller/auth.service';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileServiceService {
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+
+  submitProfileChanges(): Observable<any> {
+    const headers = this.authService.getHeaders();
+
+    return this.http.post(`${this.apiUrl}/accountManagement/updateAccount`, { headers: headers});
+  }
 }
