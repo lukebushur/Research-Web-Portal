bff0a761dc1e09cdb1d1132ae7c19122426b1a60
Sean Tierney <stierney0505@gmail.com>
2024-03-04 19:55:26 -0600

    View applications (#53)
    * Fix for student application status buttons for faculty dashboard and view applicant.
    
    * View application componenet for faculty - search can now choose number per page and page number - added small helper function for network i/o optimization - github workflow for angular (WIP)
    
    * Forgor
    
    * Update client.yaml to test headless chrome
    
    * Network I/O optimizer helper function. This standardizes how requests to retrieve records from the database are made. Not for database requests that retrieve multiple records, only requests that retrieve a single record.
    
    * Search service for accessing the search route
    
    * Fix for angular unit tests
    
    * update for PR
    
    * pr edit
    
    * pr fix
    
    * Merge Conflict issues fix
    
    * fixed getMajorsIssue
    
    * Workflow to run one unit test run once when there is an active pr
    
    * Forgot to put it on the server.yaml
    
    * Temp commit for start of faculty view specific project
    
    * Set up view project component for Nick to work on
    
    * remove accidental creation of component
    
    * Small PR to provide luke with the updated reset password routes + optimization of industry routes I/O and update the unit tests to ensure they work with the updated routes.
    
    * View Applications Table in Student Dashboard
    
    * Finished Student View Application from student's prespective
    
    * minor css/html improvements to view student application page
    
    * update for unit test fix
    
    * pr fix
    
    * Update to searchDataStructures to accept empty queries
    
    * Angular Unit Test Fixes
    
    * Replace removed semicolon

--- client/src/app/components/student-view-application/student-view-application.component.spec.ts
+++ client/src/app/components/student-view-application/student-view-application.component.spec.ts
@@ -1,10 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommonModule } from '@angular/common'; // Import CommonModule
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { StudentViewApplicationComponent } from '../student-view-application/student-view-application.component';
 import { StudentDashboardService } from 'src/app/controllers/student-dashboard-controller/student-dashboard.service';
+import { SearchProjectService } from 'src/app/controllers/search-project-controller/search-project.service';
 import { DateConverterService } from 'src/app/controllers/date-converter-controller/date-converter.service';
 import { RouterModule } from '@angular/router';
-import { SpinnerComponent } from '../spinner/spinner.component';
+import { Component, NgModule } from '@angular/core';
+
+@Component({ standalone: true, selector: 'app-spinner', template: '' })
+class SpinnerSubComponent {}
+
 
 describe('StudentViewApplicationComponent', () => {
   let component: StudentViewApplicationComponent;
@@ -13,14 +20,16 @@ describe('StudentViewApplicationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [StudentViewApplicationComponent],
-      imports: [HttpClientTestingModule, RouterModule.forRoot([])], // Import RouterModule
+
+      imports: [HttpClientTestingModule, RouterModule.forRoot([]), SpinnerSubComponent, CommonModule], // Add CommonModule here
       providers: [
+        StudentDashboardService, // Provide StudentDashboardService here
         {
-          provide: StudentDashboardService,
+          provide: DateConverterService,
           useValue: {}
         },
         {
-          provide: DateConverterService,
+          provide: SearchProjectService,
           useValue: {}
         }
       ]
@@ -34,6 +43,5 @@ describe('StudentViewApplicationComponent', () => {
   });
 
   it('should create', () => {
-    expect(component);
   });
 });
