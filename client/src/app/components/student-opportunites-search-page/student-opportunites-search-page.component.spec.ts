c57e77c77e03e6caf5aaedd165a607bcbc54adcb
nachobrothernic <35301229+nachobrothernic@users.noreply.github.com>
2024-03-08 19:39:34 -0500

    Fixing test

--- client/src/app/components/student-opportunites-search-page/student-opportunites-search-page.component.spec.ts
+++ client/src/app/components/student-opportunites-search-page/student-opportunites-search-page.component.spec.ts
@@ -3,6 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StudentOpportunitesSearchPageComponent } from './student-opportunites-search-page.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 describe('StudentOpportunitesSearchPageComponent', () => {
   let component: StudentOpportunitesSearchPageComponent;
@@ -11,7 +16,7 @@ describe('StudentOpportunitesSearchPageComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [StudentOpportunitesSearchPageComponent],
-      imports: [HttpClientTestingModule, FormsModule],
+      imports: [HttpClientTestingModule, FormsModule, MatFormFieldModule, CdkAccordionModule, FormsModule, MatInputModule, BrowserAnimationsModule, MatPaginatorModule],
     });
     fixture = TestBed.createComponent(StudentOpportunitesSearchPageComponent);
     component = fixture.componentInstance;
