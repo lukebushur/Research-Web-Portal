451de26a6002bc3c3525ddd74a985075de60d228
lukebushur <luke7ajb@gmail.com>
2024-03-24 15:25:10 -0400

    Bootstrap the project using standalone APIs

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,20 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { TableDataSharingService } from './app/_helpers/table-data-sharing/table-data-sharing.service';
+import { LoadingInterceptor } from './app/loading.interceptor';
+import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 
-import { AppModule } from './app/app.module';
-import {  } from './app/_helpers/auth/app.auth';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(AppRoutingModule),
+    {
+      provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
+    },
+    TableDataSharingService,
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations()
+  ]
+}).catch(err => console.error(err));
