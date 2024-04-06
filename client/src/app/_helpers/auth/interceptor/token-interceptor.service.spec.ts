29b36f304b691f9c9078b995937606298a03584f
lukebushur <luke7ajb@gmail.com>
2024-04-06 19:01:31 -0400

    TokenInterceptor unit tests

--- client/src/app/_helpers/auth/interceptor/token-interceptor.service.spec.ts
+++ client/src/app/_helpers/auth/interceptor/token-interceptor.service.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TokenInterceptor } from './token-interceptor.service';
+import { HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-describe('AuthService', () => {
-  let service: TokenInterceptor;
+describe('TokenInterceptor', () => {
+  let interceptor: TokenInterceptor;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TokenInterceptor);
+    interceptor = new TokenInterceptor();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(interceptor).toBeTruthy();
+  });
+
+  it('created request should have the Authorization header set', () => {
+    const next: HttpHandler = {
+      handle: () => {
+        return new Observable<HttpEvent<any>>(subscriber => {
+          subscriber.complete();
+        });
+      }
+    };
+    const requestMock = new HttpRequest('GET', '/test');
+
+    spyOn(next, 'handle').and.callThrough();
+    interceptor.intercept(requestMock, next).subscribe();
+    expect(next.handle).toHaveBeenCalledWith(requestMock.clone({
+      setHeaders: {
+        Authorization: `Bearer ${localStorage.getItem("jwt-auth-token")}`
+      }
+    }));
   });
 });
