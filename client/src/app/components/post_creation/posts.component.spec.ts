434c313150252d7bd49146d22f9a7cc8b2883d73
nachobrothernic <35301229+nachobrothernic@users.noreply.github.com>
2024-04-08 12:53:07 -0400

    Add SOME of the create-project unit tests, it needs work because it's being a butt

--- client/src/app/components/post_creation/posts.component.spec.ts
+++ client/src/app/components/post_creation/posts.component.spec.ts
@@ -1,22 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { PostProjectComponent } from './posts.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatRadioModule } from '@angular/material/radio';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Component, Input } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { QuestionData } from 'src/app/_models/projects/questionData';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { ProjectData } from 'src/app/_models/projects/projectData';
+import { of } from 'rxjs';
+import { AuthService } from 'src/app/controllers/auth-controller/auth.service';
+import { MatSelectHarness } from '@angular/material/select/testing';
 
 @Component({ standalone: true, selector: 'app-spinner', template: '' })
 class SpinnerSubComponent { }
@@ -27,11 +34,40 @@ class CreateQuestionsFormStubComponent {
   @Input() questionsData?: QuestionData[];
 }
 
+const testProjectData: ProjectData = {
+  professorName: 'Test Professor',
+  professorEmail: 'testemail@email.com',
+  projectID: '123',
+  projectName: 'Test Project',
+  description: 'This project is for testing.',
+  categories: ['Technology', 'Documentation', 'Writing'],
+  GPA: 2.0,
+  majors: ['Computer Science', 'Theatre'],
+  posted: 'Fri Feb 16 2024',
+  deadline: 'Sun Mar 17 2024',
+  questions: []
+};
+
+const testGetMajorResponse = {
+  success: {
+    majors: [
+      'Computer Science',
+      'Music',
+      'Biology',
+    ]
+  }
+};
+
 describe('PostProjectComponent', () => {
   let component: PostProjectComponent;
   let fixture: ComponentFixture<PostProjectComponent>;
 
-  beforeEach(() => {
+  let loader: HarnessLoader;
+
+  const authService = jasmine.createSpyObj('AuthService', ['getMajors']);
+  let authSpy = authService.getMajors.and.returnValue(Promise.resolve(of(testGetMajorResponse)));
+
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [
         SpinnerSubComponent,
@@ -51,42 +87,115 @@ describe('PostProjectComponent', () => {
         BrowserAnimationsModule,
         PostProjectComponent,
       ],
-      providers: [provideRouter([])]
+      providers: [provideRouter([]), {
+        provide: AuthService,
+        useValue: authService
+      }]
     });
     fixture = TestBed.createComponent(PostProjectComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
+    await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // Set all form values via HTML to random data and check and see if they were properly submitted
-  // it('should check for data', () => {
-  //   // Should set the HTML values to random data
-  //   const title = 'test title';
-  //   const description = 'test description';
-  //   const category = 'test category';
-  //   const startDate = new Date();
-  //   const endDate = new Date();
-  //   const questions = [{ question: 'test question', type: 'text', required: true}];
+  it('should say project name is invalid when empty', async () => {
+    // use harness here
+    const AllInputHarnesses = await loader.getAllHarnesses(MatInputHarness);
+    const projectNameInput = await AllInputHarnesses.find(async (input) => await input.getName() === 'Project Name');
+    expect(projectNameInput).toBeTruthy();
+    expect(component.projectForm.get("details")?.get("projectName")?.invalid).withContext('should be empty & invalid').toBe(true);
+    // Now let's set the input to something
+    await projectNameInput?.setValue(testProjectData.projectName);
+    expect(component.projectForm.get("details")?.get("projectName")?.value).toBe(testProjectData.projectName);
+    expect(component.projectForm.get("details")?.get("projectName")?.invalid).withContext('should have text now').toBe(false);
+  })
 
-  //   // Now we want to set the form values to the random data
-  //   component.projectForm.setValue({
-  //     details: {
-  //       projectName: title,
-  //       description: description,
-  //       majors: null,
-  //       categories: [],
-  //       deadline: endDate,
-  //       responsibilities: null,
-  //       GPA: null
-  //     },
-  //     questionsGroup: {
-  //       questions: questions
-  //     }
-  //   });
-  // })
+  it('should say description is invalid when empty', async () => {
+    // use harness here
+    const AllInputHarnesses = await loader.getAllHarnesses(MatInputHarness);
+    const descriptionInput = AllInputHarnesses[1];
+    expect(descriptionInput).toBeTruthy();
+    await descriptionInput?.setValue('');
+    expect(component.projectForm.get("details")?.get("description")?.invalid).withContext('should be empty & invalid').toBe(true);
+    // Now let's set the input to something
+    expect(descriptionInput).toBeTruthy();
+    await descriptionInput?.setValue('');
+    await descriptionInput?.setValue(testProjectData.description);
+    expect(component.projectForm.get("details")?.get("description")?.value).toBe(testProjectData.description);
+    expect(component.projectForm.get("details")?.get("description")?.invalid).withContext('should have text now').toBe(false);
+  })
+
+  it('should expect majors to be valid', async () => {
+    // use harness here
+    const InputHarness = await loader.getHarness(MatSelectHarness);
+    await InputHarness.open();
+    const options = await InputHarness.getOptions();
+    expect(options.length).toBeGreaterThan(0);
+    // Click first option in list
+    await options[0].click();
+    expect(component.projectForm.get("details")?.get("majors")?.value).toContain('Biology'); // weird type error here
+  })
+
+  // Harness does not like this lol
+  // it('should pick a date and expect it to be valid', async () => {
+  //   // use harness here
+  //   const InputHarness = await loader.getHarness(MatInputHarness);
+  //   const expectedDeadline = new Date(testProjectData.deadline);
+  //   await InputHarness.setValue(expectedDeadline.toISOString()); // Fix: Convert Date object to string
+  //   expect(component.projectForm.get("details")?.get("deadline")?.value).toEqual(expectedDeadline);
+  // });
+
+  it('should expect GPA to be valid', async () => {
+    // use harness here
+    const AllInputHarnesses = await loader.getAllHarnesses(MatInputHarness);
+    const Input = AllInputHarnesses[4];
+    expect(Input).withContext('should exist').toBeTruthy();
+    await Input?.setValue('2.0');
+    expect(component.projectForm.get("details")?.get("GPA")?.value).toBe('2.0');
+    expect(component.projectForm.get("details")?.get("GPA")?.invalid).toBe(false);
+    await Input?.setValue('5');
+    expect(component.projectForm.get("details")?.get("GPA")?.invalid).toBe(true);
+  });
+
+  it('should set categories', async () => {
+    const AllInputHarnesses = await loader.getAllHarnesses(MatInputHarness);
+    const Input = AllInputHarnesses[3];
+    expect(Input).withContext('should exist').toBeTruthy();
+    await Input?.setValue('Category');
+  })
+
+  // TODO Figure out why questions are being such a butt for no reason at all
+  // Set to exactly what is being sent through from the server. No go.
+  // Set to an empty array. No go.
+  // Made fake questions. No go.
+  // I can't please it. I don't know what to do here.
+  // Disabling it.
+
+  // it('should submit the project', fakeAsync(() => {
+  //   tick();
+  //   // Convert this to individual 
+  //   // Like 
+  //   component.projectForm.get("details")?.get("projectName")?.setValue(testProjectData.projectName);
+  //   component.projectForm.get("details")?.get("description")?.setValue(testProjectData.description);
+    
+  //   let formBuilder = new FormBuilder();
+  //   component.categories.push(formBuilder.control('Technology'));
+
+  //   component.projectForm.get("details")?.get("GPA")?.setValue(String(testProjectData.GPA));
+  //   component.projectForm.get("details")?.get("majors")?.setValue(['Computer Science']);
+  //   component.projectForm.get("details")?.get("deadline")?.setValue(new Date(testProjectData.deadline));
+  //   component.projectForm.get("details")?.get("responsibilities")?.setValue(null);
+  //   // I hate form groups dude
+  //   component.questionsGroup.get("questions")?.setValue([]);
+
+  //   expect(component.details.get("questionsGroup")?.valid).withContext('to be valid').toBe(true);
+   
+  //   expect(component.projectForm.valid).toBe(true);
+  // }))
 
 });
