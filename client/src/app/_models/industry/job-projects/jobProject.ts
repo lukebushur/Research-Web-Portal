12950671e7eff998eb7b91b97a01c4a8f8ba4277
lukebushur <luke7ajb@gmail.com>
2024-04-29 13:49:07 -0400

    Updated fields for JobProject interface

--- client/src/app/_models/industry/job-projects/jobProject.ts
+++ client/src/app/_models/industry/job-projects/jobProject.ts
@@ -1,10 +1,12 @@
 export interface JobProject {
+  _id: string;
   title: string;
   description: string;
   skillsAssessed: string;
   eta: string;
   deadline: Date;
-  materials: string[];
+  materials?: string[];
   submissionType: 'text' | 'file';
   fileTypes?: string[];
+  dateCreated: Date;
 };
