81647a78003fba8346b99840252ef1c98af9ff6a
lukebushur <luke7ajb@gmail.com>
2025-01-14 17:45:19 -0500

    WIP transition of authRoutes to ES6 module

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,17 @@
 /*  This is the file for authentication and authorization routes. These routes are account type agnostic, so they can be used by either faculty or students. */
+import express from 'express';
 
-const express = require('express');
-const authController = require('../controllers/authenticate');
+import {
+    register,
+    token,
+    confirmEmailToken,
+    login,
+    getAvailableMajors,
+} from '../controllers/authenticate';
+
+import verifyToken from '../helpers/verifyToken';
 
 //API MIDDLEWARE
-const verifyToken = require('../helpers/verifyToken');
 const rateLimiter = require('../helpers/rateLimiter');
 const { registerMajorValidation } = require('../helpers/inputValidation/accountValidation');
 
@@ -14,18 +21,18 @@ const router = express.Router();
 
 //POST REGISTER - Route to create an account, utilizes the registerMajorValidation middleware to ensure that student accounts have majors
 //that align with the available majors from their university.
-router.post('/register', [registerMajorValidation], authController.register);
+router.post('/register', [registerMajorValidation], register);
 
 //POST TOKEN - Route to regenerate an access token given a refresh token and valid access token
-router.post('/token', [verifyToken], authController.token);
+router.post('/token', [verifyToken], token);
 
 //POST Confirm Email 
-router.post('/confirmEmail', [verifyToken], authController.confirmEmailToken);
+router.post('/confirmEmail', [verifyToken], confirmEmailToken);
 
 //POST Login
-router.post('/login', authController.login);
+router.post('/login', login);
 
 //GET Get available Majors - This route grabs the list of available majors from a university, which allows the frontend to popualte drop down boxes
-router.get('/getMajors', authController.getAvailableMajors)
+router.get('/getMajors', getAvailableMajors)
 
-module.exports = router;
+export default router;
