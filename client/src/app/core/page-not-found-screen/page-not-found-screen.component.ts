8e0426f7c340e03947881e510f429dae84088c14
lukebushur <luke7ajb@gmail.com>
2025-03-10 15:46:14 -0400

    styled not found page using material card

--- client/src/app/core/page-not-found-screen/page-not-found-screen.component.ts
+++ client/src/app/core/page-not-found-screen/page-not-found-screen.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-page-not-found-screen',
   templateUrl: './page-not-found-screen.component.html',
   styleUrls: ['./page-not-found-screen.component.css'],
-  standalone: true
+  imports: [MatCardModule]
 })
 export class PageNotFoundScreenComponent { }
