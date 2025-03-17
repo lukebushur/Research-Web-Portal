b3fc27fba52c62aefa157fa30399877f5061b430
lukebushur <luke7ajb@gmail.com>
2025-03-17 18:58:51 -0400

    Made QuestionData requirementType type more specific

--- client/src/app/shared/models/questionData.ts
+++ client/src/app/shared/models/questionData.ts
@@ -1,6 +1,6 @@
 export interface QuestionData {
   question: string;
-  requirementType: string;
+  requirementType: 'text' | 'check box' | 'radio button';
   required: boolean;
   choices?: string[] | null;
   questionNum?: number;
