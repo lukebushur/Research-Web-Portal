b425199a64538d40bc9c5709bed2804d7a35730e
lukebushur <luke7ajb@gmail.com>
2025-03-24 02:20:57 -0400

    Incorporated separate ProjectInformation type for external flexibility

--- client/src/app/students/student-project-description/student-project-description.component.ts
+++ client/src/app/students/student-project-description/student-project-description.component.ts
@@ -1,9 +1,22 @@
 import { Component, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { StudentProjectInfo } from '../models/student-project-info';
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
+import { QuestionData } from 'app/shared/models/questionData';
+
+interface ProjectInformation {
+  projectName: string;
+  professorName?: string;
+  posted: Date;
+  deadline: Date;
+  description: string;
+  responsibilities?: string;
+  categories: string[];
+  GPA: number;
+  majors: string[];
+  questions: QuestionData[];
+}
 
 @Component({
   selector: 'app-student-project-description',
@@ -20,5 +33,5 @@ import { MatChipsModule } from '@angular/material/chips';
 export class StudentProjectDescriptionComponent {
   readonly showProfessor = input<boolean>(true);
   readonly professorEmail = input<string>();
-  readonly project = input.required<StudentProjectInfo>();
+  readonly project = input.required<ProjectInformation>();
 }
