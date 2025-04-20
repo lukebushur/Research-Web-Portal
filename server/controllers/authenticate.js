74948e26848bb6556bff97bb828bdc4db952c413
lukebushur <luke7ajb@gmail.com>
2025-04-20 00:38:39 -0400

    undid whitespace change

--- server/controllers/authenticate.js
+++ server/controllers/authenticate.js
@@ -31,6 +31,7 @@ const login = async (req, res) => {
             if (user) {
                 //Check if the password is correct against the hashed password in the db, otherwise sends error response
                 const validatePassword = await bcrypt.compare(req.body.password, user.password);
+
                 if (validatePassword) {
                     //Generate Access and refresh tokens
                     const accessToken = generateAccessToken(user.id, user.email, user.name);
