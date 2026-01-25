c99e96ff62c2cb63ead11d116129c1377b76f6e4
Sean <stierney0505@gmail.com>
2026-01-24 22:33:35 -0600

    last commit hopefully?

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -24,7 +24,7 @@ async function dbConnect() {
     const password = encodeURIComponent(process.env.DB_PASS); // Fixes the error
     const host = process.env.DB_URL;
 
-    const dbUri = `mongodb://${username}:${password}@${host}:27017${db}&ssl=true&ssl_ca_certs=global-bundle.pem&replicaSet=rs0`;
+    const dbUri = `mongodb://${username}:${password}@${host}:27017${db}&tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
     console.log(dbUri)
     const dbName = /^\/(.+)\?/.exec(db)[1];
 
