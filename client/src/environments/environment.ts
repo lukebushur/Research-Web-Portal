d5d82d90f6c49e78143bd333428f8a4705cbf834
Sean Tierney <stierney0505@gmail.com>
2026-01-24 23:59:18 -0600

    test if set env will work

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,7 @@
+// change to port 3000 for docker deployment
+export const environment = {
+  apiUrl: 'https://research-web-portal.org/api',
+  studentType: 0,
+  facultyType: 1,
+  industryType: 2,
+};
