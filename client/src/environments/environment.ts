f8eb79529d6781c02f1bfcf5198e699cdd80cea4
Sean Tierney <stierney0505@gmail.com>
2026-02-01 01:55:39 -0600

    attempt to send env to agnular app

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,9 @@
 // change to port 3000 for docker deployment
 export const environment = {
-  apiUrl: (window as any)["env"]?.["BACKEND_URI"] || 'http://localhost:3000/api',
+
+  get apiUrl() {
+    return (window as any)["env"]?.["BACKEND_URI"] || 'http://localhost:3000/api';
+  },
   studentType: 0,
   facultyType: 1,
   industryType: 2,
