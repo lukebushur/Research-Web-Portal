d27407c91855a8f38d0859129d84f94aa6040f1c
Sean Tierney <stierney0505@gmail.com>
2026-02-01 00:19:42 -0600

    attempt to send env to agnular app

--- client/src/environments/env.template.js
+++ client/src/environments/env.template.js
@@ -1,5 +1,4 @@
 (function(window) {
   window["env"] = window["env"] || {};
-  // The placeholder ${API_URL} will be replaced by the ECS variable
-  window["env"]["apiUrl"] = "${API_URL}";
+  window["env"]["apiUrl"] = "${BACKEND_URI}";
 })(this);
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // change to port 3000 for docker deployment
 export const environment = {
-  apiUrl: (window as any)["env"]?.["apiUrl"] || 'http://localhost:3000/api',
+  apiUrl: (window as any)["env"]?.["BACKEND_URI"] || 'http://localhost:3000/api',
   studentType: 0,
   facultyType: 1,
   industryType: 2,
