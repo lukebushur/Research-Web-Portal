ab02015f4ffbfa2f8e96e6d906ec5111994d83a1
Sean Tierney <stierney0505@gmail.com>
2026-02-01 01:16:15 -0600

    attempt to send env to agnular app

--- client/run.sh
+++ client/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "The BACKEND_URI is: $BACKEND_URI"
+
 envsubst '${BACKEND_URI}' < /usr/share/nginx/html/environments/env.template.js > /usr/share/nginx/html/environments/env.js
 
 exec nginx -g 'daemon off;'
